657a6f55160cf14cd3351844d8b9ac2e47181558
Eric Miller <ericzacharymiller@gmail.com>
2022-01-15 15:08:58 -0500

    added attachments and equipment

---
+++ src/attachments.ts
@@ -0,0 +1,13 @@
+export const muzzle = ['slot_1', 'slot_2', 'slot_3', 'slot_4', 'slot_5']
+
+export const barrel = ['slot_1', 'slot_2', 'slot_3', 'slot_4', 'slot_5']
+
+export const underbarrel = ['slot_1', 'slot_2', 'slot_3', 'slot_4', 'slot_5']
+
+export const magazine = ['slot_1', 'slot_2', 'slot_3', 'slot_4', 'slot_5']
+
+export const optic = ['slot_1', 'slot_2', 'slot_3', 'slot_4', 'slot_5', 'slot_6', 'slot_7', 'slot_8', 'slot_9', 'slot_10']
+
+export const rearGrip = ['slot_1', 'slot_2', 'slot_3', 'slot_4', 'slot_5']
+
+export const stock = ['slot_1', 'slot_2', 'slot_3', 'slot_4', 'slot_5']
---
+++ src/equipment.ts
@@ -0,0 +1,3 @@
+export const tactical = ['stun grenade', 'heartbeat sensor', 'stim', 'flash grenade', 'smoke grenade', 'gas grenade', 'snapshot grenade', 'decoy grenade']
+
+export const lethal = ['throwing knife', 'semtex', 'frag grenade', 'thermite', 'c4', 'molotov cocktail', 'proximity mine', 'claymore']
